62bf24932bbb5ab1a6663d356c63ff685e99bbd2
joosep <joosephook@gmail.com>
2021-04-13 12:05:38 +0100

    Add comments regarding the possibility and applicability of using the ReplayBuffer for algorithms

--- common/replay_buffer.py
+++ common/replay_buffer.py
@@ -15,18 +15,37 @@ class ReplayBuffer:
         self.current_idx = 0
         self.current_size = 0
         # create the buffer to store info
-        self.buffers = {'o': np.zeros([self.size, self.episode_limit, self.n_agents, self.obs_shape], dtype=np.float32),
-                        'u': np.zeros([self.size, self.episode_limit, self.n_agents, 1], dtype=np.float32),
-                        's': np.zeros([self.size, self.episode_limit, self.state_shape], dtype=np.float32),
-                        'r': np.zeros([self.size, self.episode_limit, 1], dtype=np.float32),
-                        'o_next': np.zeros([self.size, self.episode_limit, self.n_agents, self.obs_shape], dtype=np.float32),
-                        's_next': np.zeros([self.size, self.episode_limit, self.state_shape], dtype=np.float32),
-                        'avail_u': np.zeros([self.size, self.episode_limit, self.n_agents, self.n_actions], dtype=np.float32),
-                        'avail_u_next': np.zeros([self.size, self.episode_limit, self.n_agents, self.n_actions], dtype=np.float32),
-                        'u_onehot': np.zeros([self.size, self.episode_limit, self.n_agents, self.n_actions], dtype=np.float32),
-                        'padded': np.zeros([self.size, self.episode_limit, 1], dtype=np.float32),
-                        'terminated': np.zeros([self.size, self.episode_limit, 1], dtype=np.float32)
-                        }
+        assert True, "Really think about the buffer thing: does zero padding"\
+                      "make sense for ALL of these? It makes sense for inputs"\
+                      "to the network, but does it make sense for the action?"\
+                      "The other agents aren't always taking the 0 action, so it"\
+                      " would be wrong to say they do..."
+
+
+        self.buffers = {
+            # zero padding ok because it is just input data, 0 means "missing data"
+            'o': np.zeros([self.size, self.episode_limit, self.n_agents, self.obs_shape], dtype=np.float32),
+            # action: zero padding doesn't make sense because the non-existent agents would be taking action 0,
+            'u': np.zeros([self.size, self.episode_limit, self.n_agents, 1], dtype=np.float32),
+            # zero padding ok because it is just input data, 0 means "missing data"
+            's': np.zeros([self.size, self.episode_limit, self.state_shape], dtype=np.float32),
+            # no zero padding needed
+            'r': np.zeros([self.size, self.episode_limit, 1], dtype=np.float32),
+            # zero padding ok because it is just input data, 0 means "missing data"
+            'o_next': np.zeros([self.size, self.episode_limit, self.n_agents, self.obs_shape], dtype=np.float32),
+            # zero padding ok because it is just input data, 0 means "missing data"
+            's_next': np.zeros([self.size, self.episode_limit, self.state_shape], dtype=np.float32),
+            # zero padding would be ok because avail_u is the mask of available actions, zero means an action is not available
+            'avail_u': np.zeros([self.size, self.episode_limit, self.n_agents, self.n_actions], dtype=np.float32),
+            # zero padding would be ok because avail_u_next is the mask of available actions, zero means an action is not available
+            'avail_u_next': np.zeros([self.size, self.episode_limit, self.n_agents, self.n_actions], dtype=np.float32),
+            # zero padding would be ok because adding more zeroes just extends the space of possible actions
+            'u_onehot': np.zeros([self.size, self.episode_limit, self.n_agents, self.n_actions], dtype=np.float32),
+            # zero padding not needed
+            'padded': np.zeros([self.size, self.episode_limit, 1], dtype=np.float32),
+            # zero padding not needed
+            'terminated': np.zeros([self.size, self.episode_limit, 1], dtype=np.float32)
+        }
         if self.args.alg == 'maven':
             self.buffers['z'] = np.empty([self.size, self.args.noise_dim], dtype=np.float32)
         # thread lock
@@ -34,21 +53,21 @@ class ReplayBuffer:
 
         # store the episode
     def store_episode(self, episode_batch):
-        batch_size = episode_batch['o'].shape[0]  # episode_number
-        batch_size, timesteps, n_agents, obs_size = episode_batch['o'].shape
-        *rest, n_actions, _ = episode_batch['u'].shape
+        batch_size, time_limit, n_agents, _ = episode_batch['o'].shape  # episode_number
+        batch_size, time_limit, _, n_actions = episode_batch['avail_u'].shape
+
         with self.lock:
             idxs = self._get_storage_idx(inc=batch_size)
             # store the informations
             self.buffers['o'][idxs, :, :n_agents, :] = episode_batch['o']
-            self.buffers['u'][idxs, :, :n_actions, :] = episode_batch['u']
+            self.buffers['u'][idxs, :, :n_agents, :] = episode_batch['u']
             self.buffers['s'][idxs] = episode_batch['s']
             self.buffers['r'][idxs] = episode_batch['r']
             self.buffers['o_next'][idxs, :, :n_agents, :] = episode_batch['o_next']
             self.buffers['s_next'][idxs] = episode_batch['s_next']
-            self.buffers['avail_u'][idxs, :, :n_actions, :] = episode_batch['avail_u']
-            self.buffers['avail_u_next'][idxs, :, :n_actions, :] = episode_batch['avail_u_next']
-            self.buffers['u_onehot'][idxs, :, :n_actions, :] = episode_batch['u_onehot']
+            self.buffers['avail_u'][idxs, :, :n_agents, :n_actions] = episode_batch['avail_u']
+            self.buffers['avail_u_next'][idxs, :, :n_agents, :n_actions] = episode_batch['avail_u_next']
+            self.buffers['u_onehot'][idxs, :, :n_agents, :n_actions] = episode_batch['u_onehot']
             self.buffers['padded'][idxs] = episode_batch['padded']
             self.buffers['terminated'][idxs] = episode_batch['terminated']
             if self.args.alg == 'maven':
