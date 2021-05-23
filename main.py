c3cb453a60d85fdebcadd0b9dd98981708e6b6a6
joosep <joosephook@gmail.com>
2021-05-23 14:35:29 +0100

    Reset epsilon

--- main.py
+++ main.py
@@ -133,6 +133,7 @@ if __name__ == '__main__':
 
             runner.patience = 20
             runner.run(i)
+            runner.rolloutWorker.epsilon = args.epsilon
             runner.agents.policy.reset_optimiser()
             runner.agents.policy.load_target()
             runner.train_env.close()
